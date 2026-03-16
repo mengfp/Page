b63f2f1579c3c1e216ca7b193bb95af48094c3c8
mengfp <mengfp@sina.com>
2026-03-16 02:27:10 -0400

    bump up version

--- version.py
+++ version.py
@@ -1,6 +1,6 @@
 """Single place for app name + version (About, packaging)."""
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 APP_NAME = "Page"
 
 # Decrypted .page root "id" must equal this. Root "version" is always __version__ (no separate schema version).
